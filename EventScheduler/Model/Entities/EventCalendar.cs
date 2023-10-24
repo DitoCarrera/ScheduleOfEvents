33ab2967451c1257175839360860d1c120d6a70c
Vitor Carrera <vitorcarreraya@gmail.com>
2023-10-24 19:34:00 -0300

    Criando as classe executável

---
+++ EventScheduler/Model/Entities/EventCalendar.cs
@@ -0,0 +1,76 @@
+﻿using EventScheduler.Model.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventScheduler.Model.Entities
+{
+    internal class EventCalendar : IEventSheduler
+    {
+        public List<Event> Events { get; private set; } = new List<Event>();
+
+        public void AddEvent(Event @event)
+        {
+            if (Events.Any(e =>
+            (@event.StartDate >= e.StartDate && @event.StartDate <= e.EndDate) ||
+            (@event.EndDate >= e.StartDate && @event.EndDate <= e.EndDate) ||
+            (@event.StartDate <= e.StartDate && @event.EndDate >= e.EndDate)))
+            {
+                throw new ArgumentException("Time conflict: An event is already scheduled for the same time interval.");
+            }
+            Events.Add(@event);
+        }
+
+        public Event FindEvent(string title, DateTime startDate)
+        {
+            Event @event = Events.FirstOrDefault(e => e.Title == title && e.StartDate == startDate);
+
+            return @event;
+        }
+
+        public List<Event> GetEventsForDate(DateTime date)
+        {
+            if (Events.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid operation: The list of events is empty.");
+            }
+
+            if (date <= DateTime.Now)
+            {
+                throw new InvalidOperationException("Invalid operation: The date is in the past or equal to the current date.");
+            }
+
+            List<Event> eventsForDate = Events.Where(e => e.StartDate == date.Date).OrderBy(e => e.StartDate).Take(10).ToList();
+
+            return eventsForDate;
+        }
+
+        public void RemoveEvent(string title, DateTime startDate)
+        {
+            if (Events.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid operation: The list of events is empty.");
+            }
+
+            Events.Remove(FindEvent(title, startDate));  
+        }
+
+        public List<Event> SortEvents(int numberOfEvents)
+        {
+            if (Events.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid operation: The list of events is empty.");
+            }
+
+            List<Event> events = Events;
+
+            events.Sort();
+            return events.Skip(numberOfEvents).Take(10).ToList();
+           
+        }
+
+        
+    }
+}
---
+++ EventScheduler/Program.cs
@@ -0,0 +1,112 @@
+﻿
+
+using EventScheduler.Model.Entities;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+
+Console.WriteLine("BEM VINDO A AGENDA DE EVENTOS!!!");
+
+EventCalendar events = new EventCalendar(); 
+Event @event = null;
+List<Event> eventsFind = null;
+Boolean exit = false;
+
+while (!exit)
+{
+    
+    try {
+        Console.WriteLine("\nOpções: ");
+        Console.WriteLine("[ 1 ] Add");
+        Console.WriteLine("[ 2 ] Find");
+        Console.WriteLine("[ 3 ] Remove");
+        Console.WriteLine("[ 4 ] GetEventsForDate");
+        Console.WriteLine("[ 5 ] SortEvents");
+        Console.WriteLine("[ 6 ] Exit");
+        Console.Write("Escolha sua opção: ");
+        int option = int.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                Console.Write("Title: ");
+                string title = Console.ReadLine();
+                Console.Write("Description: ");
+                string description = Console.ReadLine();
+                Console.WriteLine("Location: ");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Address: ");
+                string address = Console.ReadLine();
+                Console.Write("City: ");
+                string city = Console.ReadLine();   
+                Console.Write("State: ");
+                string state = Console.ReadLine();
+                Console.Write("PostalCode: ");
+                string postalCode = Console.ReadLine();
+                Console.Write("StartDate: ");
+                DateTime startDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+                Console.Write("EndDate: ");
+                DateTime endDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+                @event = new Event(title, description, new Location(name, address, city, state, postalCode), startDate, endDate);
+
+                events.AddEvent(@event);
+          
+                break;
+            case 2:
+                Console.WriteLine("Title: ");
+                title = Console.ReadLine();
+                Console.Write("StartDate (dd/MM/yyyy HH:mm): ");
+                startDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+                events.FindEvent(title, startDate);
+                break;
+
+            case 3:
+                Console.WriteLine("Title: ");
+                title = Console.ReadLine();
+                Console.Write("StartDate (dd/MM/yyyy): ");
+                startDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                events.RemoveEvent(title, startDate);
+                break;
+
+            case 4:
+                Console.Write("StartDate (dd/MM/yyyy): ");
+                startDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                eventsFind = events.GetEventsForDate(startDate);
+
+                foreach (Event eve in eventsFind)
+                {
+                    Console.WriteLine(eve);
+                }
+                break;
+            case 5:
+
+                int number = 0;
+                eventsFind = events.SortEvents(number);
+
+                foreach (Event eve in eventsFind)
+                {
+                    Console.WriteLine(eve);
+                }
+
+                break;
+            case 6:
+                exit = true;
+                break;
+            default:
+                throw new ArgumentException("Wrong option");
+                break;
+        }
+        Console.WriteLine("");
+
+
+    }
+    catch (InvalidOperationException e) {
+        Console.WriteLine(e.Message);
+    }
+    catch (ArgumentException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
