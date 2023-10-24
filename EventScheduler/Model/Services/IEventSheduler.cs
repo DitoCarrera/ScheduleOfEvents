54683e1df301740d15ef25400435b8488949cac5
Vitor Carrera <vitorcarreraya@gmail.com>
2023-10-24 14:56:30 -0300

    Criando a interface IEventScheduler

---
+++ EventScheduler/Model/Services/IEventSheduler.cs
@@ -0,0 +1,19 @@
+﻿using EventScheduler.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventScheduler.Model.Services
+{
+    internal interface IEventSheduler
+    {
+        public void AddEvent(Event @event);
+        public Event FindEvent(string title, DateTime startDate);
+        public void RemoveEvent(string title, DateTime startDate);
+        public List<Event> GetEventsForDate(DateTime date);
+        public List<Event> SortEvents(int numberOfEvents);
+
+    }
+}
